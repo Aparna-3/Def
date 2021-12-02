88a299e2a9882d9203a0e3f99ff34a9494addec5
Aparna-3 <93812583+Aparna-3@users.noreply.github.com>
2021-12-02 10:59:44 +0530

    Add files via upload

---
+++ Def.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class Def {
+    public static void main(String args[]){
+        int num1=5,num2=2,subtraction;
+        subtraction=num1-num2;
+        System.out.println("the subtraction is"+subtraction);
+    }
+    
+}
+
